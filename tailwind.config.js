f692f98f96b320c1440d17fc38d3ac7bc87db9e5
Will Clarke <will@clarkedevelopment.co.uk>
2024-01-06 00:06:44 +0000

    about me

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // darkMode: 'class',
   content: [
     "./index.html"
   ],
   theme: {
     extend: {},
+    colors: {
+      'gunmetal': "#293132",
+      'offwhite': "#EBEBEB",
+      'steel': "#3E7CB1"
+    }
   },
   plugins: [],
 }
