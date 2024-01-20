8cad28391f69687de5724d38defce50d7f3a58b4
Will Clarke <will@clarkedevelopment.co.uk>
2024-01-20 18:39:39 +0000

    start on changing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      'gunmetal': "#293132",
-      'offwhite': "#EBEBEB",
+      'gunmetal': "#2D2F39",
+      'offwhite': "#D2D3DB",
       'steel': "#3E7CB1",
-      'lightsteel': '#78A3C8'
+      'lightsteel': '#E0AFA1',
+
+      'dark': '#24262E',
+      'gray': '#2D2F39',
+      'light': '#D2D3DB',
+      'primary': '',
+      'secondary': '',
+      'accent': ''
     }
   },
   plugins: [],
